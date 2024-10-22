1b834e22cfcbf37c7cd5959ef29788df509bc5dd
rod773 <rodrigoarcadio1@gmail.com>
2024-10-22 14:34:02 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { AppBar, IconButton, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+} from "@mui/material";
 import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
 
 const Header = () => {
@@ -7,9 +14,27 @@ const Header = () => {
     <>
       <AppBar position="static">
         <Toolbar>
-          <IconButton>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+          >
             <LocalLibraryIcon />
           </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            Typography
+            sx={{ flexGrow: 1 }}
+          >
+            MUI LOGIN
+          </Typography>
+          <Box>
+            <Button color="inherit">SignIn</Button>
+            <Button color="inherit">SignIn-Shadow</Button>
+            <Button color="inherit">SignIn-Image</Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </>

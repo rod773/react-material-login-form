99681be646db8db3c00fb373cf6deb7a5eaa8b35
rod773 <rodrigoarcadio1@gmail.com>
2024-10-22 14:03:58 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { AppBar, IconButton, Toolbar } from "@mui/material";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton>
+            <LocalLibraryIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default Header;
